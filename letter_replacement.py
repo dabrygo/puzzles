3fb799b929f015a2a9209dd19863bafa45e91f55
Daniel <dbgodfrey89@msn.com>
2020-03-22 16:28:41 -0600

    Encode a phrase

--- letter_replacement.py
+++ letter_replacement.py
@@ -1,20 +1,25 @@
 # Replaces each letter in a string with a neighboring one in the alphabet
 
 import random
+import re
 from string import ascii_uppercase as ABC
 
 
 def letter_replacement(string):
-    encoded = ""
+    new_words = []
     string = string.upper()
-    for c in string:
-        i = ABC.find(c)
-        before = ABC[i-1]
-        after = ABC[i+1]
-        new_c = random.choice([before, after])
-        encoded += new_c
-    return encoded
+    for match in re.finditer('\w+', string):
+        new_word = ""
+        word = match.group(0)
+        for c in word:
+            i = ABC.find(c)
+            before = ABC[i-1]
+            after = ABC[i+1]
+            new_c = random.choice([before, after])
+            new_word += new_c
+        new_words.append(new_word)
+    return '|'.join(new_words)
 
-print(letter_replacement("Daniel"))
+print(letter_replacement("To be or not to be. That is the question."))
 
 
