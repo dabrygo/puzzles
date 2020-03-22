c3a8b5b4e81b91a2057977f09d6efb707ad15110
Daniel <dbgodfrey89@msn.com>
2020-03-22 17:09:20 -0600

    Decode an letter-replacement encoded phrase
    Find all possible combinations of each word.
    There are `2**n` options, where `n` is length
    of the encoded word. Test if each word or its
    root word is an English word. Return a string
    with these translations, indicating when an
    encoded word has more than one translation.

--- letter_replacement.py
+++ letter_replacement.py
@@ -1,11 +1,18 @@
 # Replaces each letter in a string with a neighboring one in the alphabet
 
+from nltk.corpus import words
+from nltk.stem.snowball import SnowballStemmer
 import random
 import re
+import pprint
 from string import ascii_uppercase as ABC
 
 
-def letter_replacement(string):
+WORDS = set(words.words())
+STEMMER = SnowballStemmer('english')
+
+
+def encode(string):
     new_words = []
     string = string.upper()
     for match in re.finditer('\w+', string):
@@ -20,6 +27,43 @@ def letter_replacement(string):
         new_words.append(new_word)
     return '|'.join(new_words)
 
-print(letter_replacement("To be or not to be. That is the question."))
+
+def attempt_words(word):
+    n = len(word)
+    options = [bin(i)[2:].zfill(n) for i in range(2**n)]
+    words = []
+    for i, option in enumerate(options):
+        new_word = ''
+        for j, c in enumerate(word):
+            choice = option[j]
+            k = ABC.find(c)
+            if choice == '0':
+                new_c = ABC[k-1]
+            else:
+                new_c = ABC[(k+1)%len(ABC)]
+            new_word += new_c
+        words.append(new_word)
+    return words
 
 
+def decode(string):
+    words = string.split('|')
+    decoded = []
+    for word in words:
+        attempts = attempt_words(word)
+        lowered = [word.lower() for word in attempts]
+        successes = [word for word in lowered if word in WORDS or STEMMER.stem(word) in WORDS]
+        if len(successes) == 1:
+            translation = successes[0]
+        elif len(successes) > 1:
+            translation = f"({'|'.join(successes)})"
+        else:
+            raise ValueError(f"Could not translate '{word}'")
+        decoded.append(translation)
+    return ' '.join(decoded)
+
+
+encoded = encode("To be or not to be. That is the question.")
+print(encoded)
+decoded = decode(encoded)
+print(decoded)
